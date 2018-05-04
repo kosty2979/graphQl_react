11b535d32d7fd859de014fe81375187516ec499a
kosty2979 <kosty2979@gmail.com>
2018-05-04 12:35:56 +0300

    added: start graphQl server & and json server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./graphQL/schema')
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Listening port 4000')
+});
