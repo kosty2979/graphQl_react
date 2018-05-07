57b03f55dccf4d98f45df5aa6290e2866249d100
kosty2979 <kosty2979@gmail.com>
2018-05-07 12:22:59 +0300

    35 react component

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 import ApolloClient  from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 
+import SongList from './components/songList';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   )
 };
