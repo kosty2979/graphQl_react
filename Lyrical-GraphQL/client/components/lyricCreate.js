3431d7db9e909c8ab279216d6833464c6e7217d9
kosty2979 <kosty2979@gmail.com>
2018-05-15 16:33:13 +0300

    65 caching with dataIdFromObject(indentifying records)

--- Lyrical-GraphQL/client/components/lyricCreate.js
+++ Lyrical-GraphQL/client/components/lyricCreate.js
@@ -39,6 +39,7 @@ mutation AddLyricToSong($content: String, $songId:ID){
   addLyricToSong(content:$content, songId:$songId){
     id
     lyrics{
+      id
       content
     }
   }
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Router, Route, hashHistory, IndexRoute}from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
-import ApolloClient  from 'apollo-client';
+import ApolloClient from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 import App from './components/App';
 import SongList from './components/songList';
 import SongCreate from './components/songCreate';
 import SongDetail from './components/songDetail';
 
-const client = new ApolloClient({});
-import  './style/style.css'
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
+
+import './style/style.css'
+
 const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
-          <IndexRoute component={SongList} />
-          <Route path="songs/new" component={SongCreate} />
-          <Route path="songs/:id" component={SongDetail} />
+          <IndexRoute component={SongList}/>
+          <Route path="songs/new" component={SongCreate}/>
+          <Route path="songs/:id" component={SongDetail}/>
         </Route>
       </Router>
     </ApolloProvider>
@@ -27,6 +31,6 @@ const Root = () => {
 };
 
 ReactDOM.render(
-  <Root />,
+  <Root/>,
   document.querySelector('#root')
 );
