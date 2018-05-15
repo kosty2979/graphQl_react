43e36fbbbe914dc290420dbbad017ff6ed499f57
kosty2979 <kosty2979@gmail.com>
2018-05-15 14:26:34 +0300

    63 rebuild query for request lyric list

--- Lyrical-GraphQL/client/components/lyricList.js
+++ Lyrical-GraphQL/client/components/lyricList.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 
 class LyricList extends Component {
+
+  renderLyrics(){
+    return this.props.lyrics.map(({id, content})=>{
+      return(
+        <li
+          className="collection-item"
+          key={id}
+        >
+          {content}
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
-      <ul>LyricList</ul>
+      <ul className="collection">{this.renderLyrics()}</ul>
     )
   }
 }
 
-export default LyricList
+export default LyricList;
